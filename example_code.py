21cb71bc3583269af7fea2eca91da8b59a9d8691
Goktug22 <38633911+Goktug22@users.noreply.github.com>
2021-04-02 15:48:16 +0300

    Update example_code.py

--- example_code.py
+++ example_code.py
@@ -1,6 +1,7 @@
 # This program obtains the class-precedence list for given three example class hierarchies 
 # starting from the given nodes by structing those examples as a graph data structure
 # and applying iterative topologial sort on them starting from a given node
+tempvar=99
 def dummyfunct():
     print("dummy")
 
